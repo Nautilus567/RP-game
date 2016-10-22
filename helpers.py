267edae28c6380ae1e1064fabc654052ba30e6ff
Nautilus567 <erquiroga@live.cl>
2016-10-21 23:03:09 -0300

    New Helper function

--- helpers.py
+++ helpers.py
@@ -16,10 +16,10 @@ def roll_dice(diceType, force):
         roll = random.randint(1, diceType)
     if force == 1:
         print('You gently drop the dice')
-        time.sleep(1)
+        time.sleep(2)
     elif force == 10:
         print('You launch the dice with all your forces')
-        time.sleep(5)
+        time.sleep(2)
     elif 1 < force < 5:
         print('You throw the dice like a professional gambler')
         time.sleep(2)
@@ -30,3 +30,29 @@ def roll_dice(diceType, force):
         print('You throw the dice hitting the board with a spin')
         time.sleep(2)
     return roll
+
+
+def effect(score, threshold, diceType):
+    """
+    Helper function that indicates the actual effect of the dice into the player action
+    :type diceType:int
+    :param diceType: Max number capable from dice
+    :param score: Int, result from roll_dice
+    :param threshold: Int, minimal score to accomplish the action
+    :return: string, indicates if the result is a critical hit, critical failure, failure, success or in the limit
+    """
+    if score == 1:
+        # Critical Failure
+        return 'CritFail'
+    elif 1 < score < threshold:
+        # Under requirements
+        return 'Fail'
+    elif score == threshold:
+        # On Limit
+        return 'Limit'
+    elif score == diceType:
+        # Critical Hit
+        return 'CritHit'
+    else:
+        return 'Success'
+
